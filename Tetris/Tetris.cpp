0fcdbd21fc2655dacf96e7211c0a3ed15b4dd5e4
Michelle-99 <MichelleBecker2003@gmail.com>
2021-09-30 21:08:59 +0200

    Update

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <vector>
 #include <windows.h>
+#include <thread>
 
 using namespace std;
 
@@ -7,19 +9,30 @@ int nScreenWidth = 80;
 int nScreenHeight = 30;
 int nFieldWidth = 12;
 int nFieldHeight = 18;
+int nSpeedCount = 0;
+int nSpeed = 20;
 wstring tetromino[7];
+bool bKey[4];
 char* pField;
 HANDLE hConsole = 0x0;
 wchar_t* screen;
 bool bGameOver = false;
+bool bForceDown = false;
 
 wchar_t* initializeScreenBuffer();
+void initializeField();
 void initializeConsole();
-int main();
 void initializeTetrominos();
 void clear();
 bool isBorder(int x, int y);
 void startGame();
+void doGameTiming();
+void doInputReading();
+void doGameLogic();
+void render();
+void handlePlayerMovement();
+void rotate();
+void forceDown();
 
 int main()
 {
@@ -34,9 +47,11 @@ int main()
 }
 
 void startGame() {
-
     while (!bGameOver) {
-
+        doGameTiming();
+        doInputReading();
+        doGameLogic();
+        render();
     }
 }
 
@@ -69,6 +84,8 @@ wchar_t* initializeScreenBuffer() {
     screen = new wchar_t[nScreenWidth * nScreenHeight];
     for (int i = 0; i < nScreenWidth * nScreenHeight; i++)
         screen[i] = L' ';
+
+    return screen;
 }
 
 void initializeConsole() {
@@ -81,4 +98,40 @@ void initializeConsole() {
 void clear() {
     CloseHandle(hConsole);
     system("pause");
+}
+
+void doGameTiming() {
+    this_thread::sleep_for(50ms);
+    nSpeedCount++;
+    bForceDown = nSpeedCount == nSpeed;
+}
+
+void doInputReading() {
+    for (int i = 0; i < 4; i++) {
+        char cLeft = 'SADR';   // S
+        char cRight = 'A';  // A
+        char cDown = 'D';   // D
+        char cRotate = 'R'; // R
+        //bKey[i] = +(0x8000 & GetAsyncKeyState())
+    }
+}
+
+void doGameLogic() {
+
+}
+
+void handlePlayerMovement() {
+
+}
+
+void rotate() {
+
+}
+
+void forceDown() {
+
+}
+
+void render() {
+
 }
